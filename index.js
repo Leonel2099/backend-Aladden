f13797c3bd358610a7f0a496d7d5e091282ad9ca
Leonel2099 <ezequielleonel1@gmail.com>
2023-07-27 19:54:45 -0300

    Se creo una instancia de express y se seteo en el puerto 4000

--- index.js
+++ index.js
@@ -1,2 +1,9 @@
 import express from "express";
-console.log("esto es una prueba del backend");
+
+const app = express();
+
+app.set('PORT', process.env.PORT || 4000);
+
+app.listen(app.get('PORT'), ()=>{
+    console.log("esto es una prueba del backend "+ app.get('PORT'));
+})
