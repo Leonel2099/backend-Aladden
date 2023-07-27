59d38406af013a200f387e19bb24e733b2f3a77e
Leonel2099 <ezequielleonel1@gmail.com>
2023-07-27 20:50:57 -0300

    Se agrego una ruta de prueba

--- index.js
+++ index.js
@@ -15,3 +15,8 @@ app.use(express.json());
 app.use(express.urlencoded({extended:true}));
 app.use(cors());
 // app.use(morgan('dev'));
+
+// http://localhost:4000/prueba
+app.get('/prueba', (req, res)=>{
+    res.send('Esta es una prueba de mi ruta GET')
+})
