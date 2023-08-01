41461299e8cd9d254ace19a3b7b05b30795ad32c
Leonel2099 <ezequielleonel1@gmail.com>
2023-08-01 04:17:22 -0300

    Creacion del modelo de pedidos

---
+++ src/models/pedidos.js
@@ -0,0 +1,28 @@
+const mongoose = require('mongoose');
+
+const pedidoSchema = new mongoose.Schema({
+  usuario: {
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Usuario',
+    required: true,
+  },
+  fecha: {
+    type: Date,
+    default: Date.now,
+  },
+  productosDelMenu: [{
+    type: mongoose.Schema.Types.ObjectId,
+    ref: 'Producto',
+    required: true,
+  }],
+  estado: {
+    type: String,
+    required: true,
+    enum: ['Pendiente', 'En progreso', 'Entregado', 'Cancelado'],
+    default: 'Pendiente',
+  },
+});
+
+const Pedido = mongoose.model('Pedido', pedidoSchema);
+
+module.exports = Pedido;
