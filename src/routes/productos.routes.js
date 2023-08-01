07586e4054a0f782f0e582297a1f51feedc7b961
Leonel2099 <ezequielleonel1@gmail.com>
2023-08-01 16:16:21 -0300

    Se agrego la ruta y controlador del DELETE

--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,11 +1,11 @@
 import { Router } from "express";
-import {crearProducto, obtenerListaProductos, obtenerProducto} from "../controllers/productos.controllers";
+import {borrarProducto, crearProducto, obtenerListaProductos, obtenerProducto} from "../controllers/productos.controllers";
 import validarProducto from "../helpers/validarProducto";
 
 const router = Router();
 router.route("/productos").post(validarProducto, crearProducto).get(obtenerListaProductos);
 
-router.route("/productos/:id").get(obtenerProducto);
+router.route("/productos/:id").get(obtenerProducto).delete(borrarProducto);
 
 export default router;
 
