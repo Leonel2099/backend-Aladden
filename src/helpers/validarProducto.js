ad1639e882b02bde949cb281e0acaad6027aa3e3
Leonel2099 <ezequielleonel1@gmail.com>
2023-08-01 04:59:58 -0300

    Se agrego las vadilaciones del formato del modelo productos

---
+++ src/helpers/validarProducto.js
@@ -0,0 +1,47 @@
+import { check } from "express-validator";
+
+const validarProducto = [
+  check("nombreProducto")
+    .notEmpty()
+    .withMessage("El nombre del producto es obligatorio")
+    .isLength({ min: 2, max: 100 })
+    .withMessage("El nombre del producto debe contener entre 2 y 100 caracteres")
+    .trim()
+    .escape(),
+  check("estado")
+    .notEmpty()
+    .withMessage("Estado es un dato obligatorio")
+    .isIn(["disponible", "agotado", "oferta", "descatalogado"])
+    .withMessage("El estado debe ser una opcion valida"),
+  check("precio")
+    .notEmpty()
+    .withMessage("El precio es obligatorio")
+    .isNumeric()
+    .withMessage("El precio debe ser un valor numerico")
+    .custom((value) => {
+      if (value >= 200 && value <= 5000) {
+        return true;
+      } else {
+        throw new Error("El precio debe estar entre 200 y 5000");
+      }
+    }),
+  check("detalle")
+    .notEmpty()
+    .withMessage("El detalle del producto es obligatorio")
+    .isLength({ min: 30, max: 500 })
+    .withMessage("El detalle del producto debe contener entre 30 y 500 caracteres")
+    .trim()
+    .escape(),
+  check("categoria")
+    .notEmpty()
+    .withMessage("La categoria es un dato obligatorio")
+    .isIn(["entrada", "principal", "postre", "bebida"])
+    .withMessage("La categoria debe ser una opcion valida"),
+  check("imagen")
+    .notEmpty()
+    .withMessage("La url de la imagen es un dato obligatorio")
+    .matches(/^(http(s?):)([/|.|\w|\s|-])*\.(?:png|jpe?g|gif|svg)$/)
+    .withMessage("La imagen debe ser una url valida, terminada en (png|jpe?g|gif|svg)")
+];
+
+export default validarProducto;
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,9 +1,9 @@
 import { Router } from "express";
-import { crearProducto } from "../controllers/productos.controllers";
+import {crearProducto} from "../controllers/productos.controllers";
 import validarProducto from "../helpers/validarProducto";
 
 const router = Router();
-router.route("/productos").post(crearProducto);
+router.route("/productos").post(validarProducto, crearProducto)
 
 export default router;
 
