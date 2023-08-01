8fd5a4347ab7e8cdb86a2605a9223b12f89bc0f8
Leonel2099 <ezequielleonel1@gmail.com>
2023-08-01 16:10:24 -0300

    Se agrego la ruta y controlador del GET(para la traer la lista de productos)

--- src/controllers/productos.controllers.js
+++ src/controllers/productos.controllers.js
@@ -2,17 +2,30 @@ import Producto from "../models/producto";
 
 //POST
 export const crearProducto = async (req, res) => {
-   try {
+  try {
     const productoNuevo = new Producto(req.body);
     await productoNuevo.save();
     res.status(201).json({
-        mensaje: 'El producto fue creado correctamente' 
-    })
-
-   } catch (error) {
+      mensaje: "El producto fue creado correctamente",
+    });
+  } catch (error) {
     console.log(error);
     res.status(400).json({
-        mensaje: 'Error, no se pudo crear el producto'
-    })
-   }
+      mensaje: "Error, no se pudo crear el producto",
+    });
   }
+};
+
+//GET (lista de producto)
+export const obtenerListaProductos = async (req, res) => {
+  try {
+    //buscar en la BD la collection de productos
+    const productos = await Producto.find();
+    res.status(200).json(productos);
+  } catch (error) {
+    console.log(error);
+    res.status(404).json({
+      mensaje: "Error al intentar obtener los productos",
+    });
+  }
+};
--- src/routes/productos.routes.js
+++ src/routes/productos.routes.js
@@ -1,12 +1,9 @@
 import { Router } from "express";
-import {crearProducto} from "../controllers/productos.controllers";
+import {crearProducto, obtenerListaProductos} from "../controllers/productos.controllers";
 import validarProducto from "../helpers/validarProducto";
 
 const router = Router();
-router.route("/productos").post(validarProducto, crearProducto)
+router.route("/productos").post(validarProducto, crearProducto).get(obtenerListaProductos);
 
 export default router;
 
-// app.get('/prueba', (req, res )=>{
-//     res.send('Esta es una prueba de mi ruta GET')
-// })
